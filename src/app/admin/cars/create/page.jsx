fcbbc7101d976b9df158ef5fddd175aa50bbc66b
Henry <henryyanggg@gmail.com>
2026-04-12 21:22:03 +0700

    Fixing serverside calling api bug

--- src/app/admin/cars/create/page.jsx
+++ src/app/admin/cars/create/page.jsx
@@ -1,10 +1,10 @@
 import CarEditor from '@/components/CarEditor';
-import { fetchItem } from '@/lib/ServerUtils';
+import { requestServer } from '@/lib/axiosLocalInstance';
 import { cookies } from 'next/headers';
 
 export default async function CreateCarPage() {
     const token = (await cookies()).get('token')?.value;
-    const tagData = await fetchItem('get', `/api/v1/tags`, token);
+    const tagData = await requestServer('get', `/api/v1/tags`, token);
     
     return <CarEditor tagData={tagData}/>;
 }
--- src/app/admin/promos/create/page.jsx
+++ src/app/admin/promos/create/page.jsx
@@ -1,10 +1,10 @@
 import PromoEditor from '@/components/PromoEditor';
-import { fetchItem } from '@/lib/ServerUtils';
+import { requestServer } from '@/lib/axiosLocalInstance';
 import { cookies } from 'next/headers';
 
 export default async function CreatePromoPage() {
     const token = (await cookies()).get('token')?.value;
-    const carData = await fetchItem('get', `/api/v1/cars`, token);
+    const carData = await requestServer('get', `/api/v1/cars`, token);
     
     return <PromoEditor carData={carData}/>;
 }
