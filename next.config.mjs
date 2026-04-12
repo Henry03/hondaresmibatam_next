480e36e9d4bf414d61f6abe24b9f5fca0134ff7d
Henry <henryyanggg@gmail.com>
2026-04-12 23:11:32 +0700

    change port of localhost

--- next.config.mjs
+++ next.config.mjs
@@ -18,7 +18,7 @@ const nextConfig = {
             {
                 protocol: 'http',
                 hostname: 'localhost',
-                port: '4000',
+                port: '3000',
                 pathname: '/uploads/**',
             },
         ],
